1db0ca03b84ed253ab572fadf58fe6c267414ba8
rupesh.shrestha@qualyfi.co.uk <rupesh.shrestha@qualyfi.co.uk>
2023-01-31 16:44:19 +0000

    Finished the final schema

--- SchemaCreation (1).py
+++ SchemaCreation (1).py
@@ -337,4 +337,134 @@ date_dim.display()
 
 # COMMAND ----------
 
+date_dim = date_dim.withColumn('date', col('date').cast('date'))
+date_dim.display()
+
+# COMMAND ----------
+
+date_dim.dtypes
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Payment fact table
+
+# COMMAND ----------
+
+s_payment_df.display()
+
+# COMMAND ----------
+
+payment_fact = s_payment_df.join(date_dim, on='date', how='left').select('payment_id', 'rider_id', 'date_id', 'amount')
+payment_fact.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Trip fact table
+
+# COMMAND ----------
+
+s_trip_df.display()
+s_trip_df.dtypes
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Trip duration
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import unix_timestamp
+
+trip_fact = s_trip_df.withColumn('trip_duration', (unix_timestamp(col('ended_at')) - unix_timestamp(col('started_at')))/60)
+trip_fact.display()
+trip_fact.dtypes
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Adding Bike ID
+
+# COMMAND ----------
+
+trip_fact = trip_fact.join(bike_dim, on='rideable_type', how='left').drop('rideable_type')
+trip_fact.display()
+trip_fact.dtypes
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Adding date and time IDs
+
+# COMMAND ----------
+
+from pyspark.sql.functions import split, col
+
+trip_fact1 = trip_fact.withColumn('started_at', col('started_at').cast('string'))
+trip_fact1 = trip_fact1.withColumn('ended_at', col('ended_at').cast('string'))
+
+trip_fact2 = trip_fact1.withColumn('started_at_date', split(col('started_at'), ' ')[0])
+trip_fact2 = trip_fact2.withColumn('started_at_time', split(col('started_at'), ' ')[1].substr(0,5))
+
+trip_fact2 = trip_fact2.withColumn('ended_at_date', split(col('ended_at'), ' ')[0])
+trip_fact2 = trip_fact2.withColumn('ended_at_time', split(col('ended_at'), ' ')[1].substr(0,5))
+
+trip_fact2 = trip_fact2.drop('started_at', 'ended_at')
+trip_fact2.display()
+
+# COMMAND ----------
+
+#join
+trip_fact3 = trip_fact2.join(date_dim, trip_fact2.started_at_date == date_dim.date, how='left').withColumnRenamed('date_id', 'started_at_date_id').drop('date')#.drop('started_at_date', 'date')
+trip_fact3 = trip_fact3.join(date_dim, trip_fact3.ended_at_date == date_dim.date, how='left').withColumnRenamed('date_id', 'ended_at_date_id').drop('ended_at_date', 'date')
+
+trip_fact3 = trip_fact3.join(time_dim, trip_fact2.started_at_time == time_dim.time, how='left').withColumnRenamed('time_id', 'started_at_time_id').drop('started_at_time', 'time')
+trip_fact3 = trip_fact3.join(time_dim, trip_fact2.ended_at_time == time_dim.time, how='left').withColumnRenamed('time_id', 'ended_at_time_id').drop('ended_at_time', 'time')
+
+
+trip_fact3.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Adding rider age
+
+# COMMAND ----------
+
+s_rider_df.display()
+
+# COMMAND ----------
+
+riders = s_rider_df.select('rider_id', 'birthday')
+riders.display()
+
+# COMMAND ----------
+
+trip_fact4 = trip_fact3.join(riders, on='rider_id', how='left')
+trip_fact4.display()
+
+# COMMAND ----------
+
+trip_fact4.dtypes
+
+# COMMAND ----------
+
+from pyspark.sql.functions import datediff, col
+import math
+
+trip_fact5 = trip_fact4.withColumn('rider_age', (datediff(col('started_at_date'), col('birthday'))/365).cast('int'))
+trip_fact5.display()
+
+# COMMAND ----------
+
+final_trip_fact = trip_fact5.select('trip_id', 'rider_id', 'bike_id', 'start_station_id', 'end_station_id', 'started_at_date_id', 'ended_at_date_id', 'started_at_time_id', 'ended_at_time_id', 'rider_age', 'trip_duration')
+final_trip_fact.display()
+
+# COMMAND ----------
+
 
